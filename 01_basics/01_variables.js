ed5352ce95d7d6858a2d6faa36ea59a9e3f28ef6
Suryansh-rana-85 <ranasuryansh017@gmail.com>
2026-06-07 16:07:44 +0000

    font commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 12345
+let accountEmail = "harshcodes@gmail.com"
+var accountPassword = "12121212"
+accountCity = "Haridwar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
