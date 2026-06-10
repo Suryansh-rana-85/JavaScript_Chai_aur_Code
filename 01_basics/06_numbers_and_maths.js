210b90f1d61550664ae8fadd4e096b1eb0d991cd
Suryansh-rana-85 <ranasuryansh017@gmail.com>
2026-06-10 13:01:25 +0000

    Numbers_Maths

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,13 @@
+// console.log(Math);
+// console.log(Math.abs(-23));
+// console.log(Math.ceil(45.78));
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// let min = 10;
+// let max = 15;
+// let num = Math.floor(Math.random() * (max-min+1)) + min                            
+// console.log(num);
+
+// Dice Roll Game 
+let diceRoll = Math.floor(Math.random() * (6-1+1)) + 1
+console.log(diceRoll)
