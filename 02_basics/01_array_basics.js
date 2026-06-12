72be8a065274dcfa48a00b37f70147bfdd42eb61
Suryansh-rana-85 <ranasuryansh017@gmail.com>
2026-06-12 12:14:46 +0000

    Array_basics

---
+++ 02_basics/01_array_basics.js
@@ -0,0 +1,32 @@
+// console.log(myArr.join());
+// myArr.push(4);
+// console.log(myArr);
+// let x = myArr.shift();
+// console.log(myArr);
+// console.log(x);
+// console.log(myArr.includes(3))
+// console.log(myArr);
+// console.log(myArr.indexOf(2));
+// console.log(myArr[1]);
+// console.log(myArr["1"]);
+// console.log(myArr.length);
+
+// console.log(myArr.length);
+// myArr[6] = "harsh"
+// console.log(myArr);
+// console.log(myArr.length);
+// myArr.length = 3;
+// console.log(myArr);
+// console.log(myArr.length)
+
+// let myArr = [1, 2, 3, 4, 5]
+// console.log(myArr.indexOf(4));
+// console.log(myArr.slice(1, 3));
+// console.log(myArr.splice(1, 3))
+
+// let myNewArr = ["apple", "mango", "lichi", "guava", "pineapple"];
+// let keys = myNewArr.keys();
+// for(const key of keys) {
+//     console.log(`${key}: ${myNewArr[key]}`);
+// }
+
---
+++ 02_basics/02_array_basics.js
@@ -0,0 +1,27 @@
+let marvel_heroes = ["Ironman", "spiderman", "hulk"];
+let dc_heroes = ["batman", "superman", "flash"];
+
+// marvel_heroes.push(dc_heroes);
+// console.log(marvel_heroes);
+
+// let all_heroes = marvel_heroes.concat(dc_heroes);
+// console.log(all_heroes);
+
+// let flattend_array = marvel_heroes.flat(Infinity);
+// console.log(flattend_array);
+// console.log(flattend_array.join(", "));
+
+// console.log(...marvel_heroes, ...dc_heroes);
+// console.log(Array.isArray(marvel_heroes));
+// console.log(Array.isArray(new Array(2, 3, 4)));
+// console.log(Array.isArray([2, 4, 5]));
+// console.log(Array.isArray(new Int16Array([15, 33])));
+
+console.log(Array.from("harsh"))
+console.log(Array.from({name: "harsh"}))
+
+score1 = 100;
+score2 = 200;
+score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
